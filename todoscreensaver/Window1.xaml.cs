fc58a9f2c4287281bb21bd719e268d9dd0856be7
Coteh <james_c_34@live.com>
2017-04-21 17:42:58 -0400

    Close on mousemove instead of mousedown

--- todoscreensaver/Window1.xaml.cs
+++ todoscreensaver/Window1.xaml.cs
@@ -33,12 +33,16 @@ namespace todoscreensaver
         /// </summary>
         public Exception LastException { get; set; }
 
+        private bool isFirstTime;
+
         public Window1()
         {
             InitializeComponent();
 
             // load settings from registry
             Settings.Load();
+
+            isFirstTime = false;
 
             try
             {
@@ -66,12 +70,16 @@ namespace todoscreensaver
         }                
 
         /// <summary>
-        /// close app on mousedown
+        /// close app on mousemove
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        private void Window_MouseMove(object sender, MouseEventArgs e)
         {
+            if (!isFirstTime) {
+                isFirstTime = true;
+                return;
+            }
             Application.Current.Shutdown();
         }
 
